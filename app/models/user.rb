413feaa59ed013e97d7651bc7c3f2a4f43db0efa
Tan Theng Lee <thengleetan@gmail.com>
2019-09-18 15:22:17 +0800

    User login

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true,
                        length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
